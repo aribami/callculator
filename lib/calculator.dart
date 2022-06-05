c8532ae0d448163e5b6bd4cc7b7e018aefc56807
Abhirami Kirthivasan <20951a0504@iare.ac.in>
2022-06-05 14:56:02 +0530

    added example code from flutter_neumorphic

---
+++ lib/calculator.dart
@@ -0,0 +1,79 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: NeumorphicFloatingActionButton(
+        child: const Icon(Icons.add, size: 30),
+        onPressed: () {},
+      ),
+      backgroundColor: NeumorphicTheme.baseColor(context),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            NeumorphicButton(
+              onPressed: () {
+                print("onClick");
+              },
+              style: const NeumorphicStyle(
+                shape: NeumorphicShape.flat,
+                boxShape: NeumorphicBoxShape.circle(),
+              ),
+              padding: const EdgeInsets.all(12.0),
+              child: Icon(
+                Icons.favorite_border,
+                color: _iconsColor(context),
+              ),
+            ),
+            NeumorphicButton(
+                margin: const EdgeInsets.only(top: 12),
+                onPressed: () {
+                  NeumorphicTheme.of(context)!.themeMode =
+                      NeumorphicTheme.isUsingDark(context)
+                          ? ThemeMode.light
+                          : ThemeMode.dark;
+                },
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+                ),
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  "Toggle Theme",
+                  style: TextStyle(color: _textColor(context)),
+                )),
+            ],
+        ),
+      ),
+    );
+    
+  }
+  Color _iconsColor(BuildContext context) {
+    final theme = NeumorphicTheme.of(context);
+    if (theme!.isUsingDark) {
+      return theme.current!.accentColor;
+    } else {
+      return const Color.fromARGB(66, 80, 39, 39);
+    }
+  }
+
+  Color _textColor(BuildContext context) {
+    if (NeumorphicTheme.isUsingDark(context)) {
+      return Colors.white;
+    } else {
+      return Colors.black;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:callculator/calculator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +24,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -69,47 +71,21 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return const NeumorphicApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFFFFFFF),
+        lightSource: LightSource.topLeft,
+        depth: 10,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        depth: 6,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Calculator(),
     );
   }
 }
